2674678fde32956c0ff061a3d42a60095d2b19b5
ScribbleTons <ugwuokeemmanuel125@gmail.com>
2022-10-13 23:21:04 +0100

    Initial Commit

---
+++ Swift UI Slot/Swift_UI_SlotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UI_SlotApp.swift
+//  Swift UI Slot
+//
+//  Created by Emmanuel Ugwuoke on 13/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_SlotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
