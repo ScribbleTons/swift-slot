9ef4c57769b9898b9e0d7b551c7f584b577cd051
ScribbleTons <ugwuokeemmanuel125@gmail.com>
2022-10-13 23:49:31 +0100

    Initial commit

--- Swift UI Slot/ContentView.swift
+++ Swift UI Slot/ContentView.swift
@@ -8,12 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    let images = ["star", "cherry", "apple"]
+    @State var firstImage = 0
+    @State var secordImage = 1
+    @State var thirdImage = 2
+    
+    @State var credit = 0
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            Text("SwiftUI Slot").font(.largeTitle)
+            Spacer()
+            Text("Credit: \(credit)")
+            Spacer()
+            
+            HStack {
+                Spacer()
+                Image(images[firstImage]).resizable().aspectRatio( contentMode: .fit)
+                Spacer()
+                Image(images[secordImage]).resizable().aspectRatio( contentMode: .fit)
+                Spacer()
+                Image(images[thirdImage]).resizable().aspectRatio( contentMode: .fit)
+                Spacer()
+            }
+            Spacer()
+            
+            Button("Spin"){
+                
+                firstImage = Int.random(in: 0...2)
+                secordImage = Int.random(in: 0...2)
+                thirdImage = Int.random(in: 0...2)
+                
+                if firstImage == secordImage && secordImage == thirdImage {
+                    credit += 10
+                }
+            }
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            .frame(width: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/40.0/*@END_MENU_TOKEN@*/)
+            .buttonBorderShape(.roundedRectangle)
+            .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.pink/*@END_MENU_TOKEN@*/)
+            .cornerRadius(/*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+            Spacer()
         }
         .padding()
     }
